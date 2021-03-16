3362b06a90a452a8ba9baafaa5ea5069ed6701e3
ChristianSantamaria <christiancameselle@hotmail.com>
2021-03-16 12:06:47 +0100

    [ADD] base_module_uninstall_batch

---
+++ base_module_uninstall_batch/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Module Uninstall Batch",
+    "summary": """
+        Adds option to uninstall modules in batch.
+    """,
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Base",
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-server",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/ir_action_data.xml",
+    ],
+}
